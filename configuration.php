21eb9c4a9459833edd771832d96d1ff7eb044207
bberg <bjoern.berg@netways.de>
2023-08-30 16:27:22 +0200

    fixed image url

--- configuration.php
+++ configuration.php
@@ -4,5 +4,5 @@ $section = $this->menuSection('XKCD');
 $section->setIcon('img/openclipart/scissors.png');
 
 $section->add('Show Random')
-        ->setIcon('img/openclipart/shuffle.png')
+        ->setIcon('img/xkcd/shuffle.png')
         ->setUrl('xkcd/show/random');
